5f1fea8f7fc5bd5bd2885dda24ce5d5227f30e1d
Malsha Samarakoon <chamodi96samarakoon@gmail.com>
2022-10-24 09:42:23 +0530

    oic updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/mal.jpeg";
+import homeLogo from "../../Assets/me.jpg";
 import Home2 from "./Home2";
 import Type from "./Type";
 
